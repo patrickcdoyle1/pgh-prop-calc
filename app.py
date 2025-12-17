d0bea32f193e639af64bfb5717e2a5cfedeaebf3
Patrick Doyle <patrickcdoyle@gmail.com>
2025-12-17 16:35:49 -0500

    fixing num1

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def calculator():
     increase = None
     city_increase = None
     schools_increase = None
+    num1 = None
     error = None
     
     # Initialize homestead_status to hold the selected value for template persistence
