f74a1f4180f12ba6daf6820ca9a956f972d04331
Patrick Doyle <patrickcdoyle@gmail.com>
2026-01-01 14:01:45 -0500

    response headers

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Adds the X-Frame-Options header to allow embedding.
-@app.after_request
-def add_security_headers(response):
+#@app.after_request
+#def add_security_headers(response):
  #   response.headers['X-Content-Type-Options'] = 'nosniff'
   #  response.headers['X-Frame-Options'] = 'SAMEORIGIN'
   #  response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-    return response
+  #  return response
 
 # Main route for the calculator
 @app.route('/', methods=['GET', 'POST'])
